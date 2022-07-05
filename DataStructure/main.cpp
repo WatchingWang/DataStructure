55499fe35e1f78922ac56fcfcc3bae78135340e9
Watching.Wang <watching.wang@zoom.us>
2022-07-05 10:41:38 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Watching Wang on 2022/7/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
